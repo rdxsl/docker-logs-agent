b4df24a972bc310f543982b3bd15f07f0c775d02
Jack Xie <jxie@riotgames.com>
2019-04-23 23:19:06 -0700

    kind of working

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+
 	"github.com/rdxsl/docker-logs-agent/controllers"
 	_ "github.com/rdxsl/docker-logs-agent/routers"
 
@@ -15,5 +21,29 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	controllers.Version = Version
+
+	cert, err := tls.LoadX509KeyPair("conf/dev/mtls/certs/server.pem", "conf/dev/mtls/certs/server.key")
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+
+	}
+	certpool := x509.NewCertPool()
+	pem, err := ioutil.ReadFile("conf/dev/mtls/certs/ca.pem")
+	if err != nil {
+		log.Fatalf("Failed to read client certificate authority: %v", err)
+	}
+	if !certpool.AppendCertsFromPEM(pem) {
+		log.Fatalf("Can't parse client certificate authority")
+	}
+
+	config := tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    certpool,
+	}
+	config.Rand = rand.Reader
+
+	beego.BeeApp.Server.TLSConfig = &config
+
 	beego.Run()
 }
