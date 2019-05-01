0ee582b2fca0c52964a6a0f98ae6bbb1da5eddd6
Jack Xie <jxie@riotgames.com>
2019-04-30 17:31:30 -0700

    fixed bug v2

---
+++ routers/commentsRouter__________controllers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/rdxsl/docker-agent-proxy/controllers:ContainerController"] = append(beego.GlobalControllerRouter["github.com/rdxsl/docker-agent-proxy/controllers:ContainerController"],
+		beego.ControllerComments{
+			Method:           "Get",
+			Router:           `/:containerID/logs`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
